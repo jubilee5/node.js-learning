7e8793080c04a2f41135b7f8e6cdd58f62933644
jubilee5 <jubileesharma17@gmail.com>
2026-06-20 23:58:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var name = "Hello Node.js";
+
+var a= 10;
+var b = 20;
+//console.log(a+b);
+
+//console.log(name);
+
+//console.log(global);        //global object in node, which is window object in browser
+
+//console.log(this); // this returns empty object
+
+console.log(globalThis);        // globalThis is same as global. it is used a standard way to access global object
