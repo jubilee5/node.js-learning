3780e99e10d16183d54ef5560593b8900e1402bb
jubilee5 <jubileesharma17@gmail.com>
2026-06-22 23:24:44 +0530

    learned IIFE

--- Ep-01-Modules/xyz.js
+++ Ep-01-Modules/xyz.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+
+//require("./path");
+// All the code of the module is wrapped inside a function. this function is knowm as IIFE (Immediately Invoked Function Expression)
+
+//IIFE
+// (function (module, require) {
+
+//     function calculateMultiply(a, b) {
+//     const result = a * b
+//     console.log(result);       
+// }
+
+// module.exports = { calculateMultiply } 
+    
+// })();
+
+//Before let and const existed, developers used IIFEs to create a private scope so variables wouldn't leak into the global scope.
